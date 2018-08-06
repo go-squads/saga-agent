f595962942cd76c6667ed5595dbdefd5ee87d702
Reynaldi Ananda Pane <reynaldipane@Reynaldis-MacBook-Pro.local>
2018-08-06 18:32:05 +0700

    [alam/rey] refactor app.go

--- app.go
+++ app.go
@@ -23,6 +23,7 @@ func (a *App) Initialize() {
 	a.Router = mux.NewRouter()
 
 	handler := lxdclient.Handler{}
+	handler.HandlerClient.Init()
 	a.Router.HandleFunc("/api/v1/containers", handler.GetContainersHandler).Methods("GET")
 	a.Router.HandleFunc("/api/v1/container/{name}", handler.GetContainerHandler).Methods("GET")
 	a.Router.HandleFunc("/api/v1/container", handler.CreateContainerHandler).Methods("POST")
